d849099f0c3f7351820d61e213df8ea9ae0b1251
Andreas Weisethaunet <weisethaunet@gmail.com>
2016-01-29 23:51:19 +0100

    added Gamecontrollers and added more player movement

---
+++ Assets/scripts/LocalDatabase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocalDatabase : MonoBehaviour {
+
+    public float speed = 50;
+    private float apocalypseMax = 100;
+    private float apocalypseCurrent = 50;
+
+    public float getApocalypseNormalized(){
+        return apocalypseCurrent / apocalypseMax;
+    }
+
+    public float addApocalypse(float addValue){
+        return apocalypseCurrent += addValue;
+    }
+}
--- Assets/scripts/playerMove.cs
+++ Assets/scripts/playerMove.cs
@@ -3,20 +3,49 @@ using System.Collections;
 
 public class playerMove : MonoBehaviour {
 
+    bool isAttacking;
+    bool isCrounching;
     bool isGrounded;
     Rigidbody rb;
+    Vector3 position;
+    Transform tf;
+    Transform scalepivot;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        tf = GetComponent<Transform>();
         isGrounded = false;
+        isAttacking = false;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.X)){
+            isAttacking = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl)){
+            isAttacking = false;
+        }
+
         if (Input.GetKeyDown("space") && isGrounded){
             rb.AddForce(new Vector3(0, 300.0f,0));
-        }        
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl)){
+            tf.localScale -= new Vector3(0, 0.5f, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl)){
+            tf.localScale += new Vector3(0, 0.5f, 0);
+        }
+
+
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * 1 * Time.deltaTime;
+
     }
 
     void OnCollisionEnter(Collision other){
