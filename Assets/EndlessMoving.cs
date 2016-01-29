333225b87f78bd6bcd012d6bee3dd92733402d1b
Halvor Bakken Smedås <stektpotet@gmail.com>
2016-01-30 00:35:18 +0100

    Added image effects

--- Assets/EndlessMoving.cs
+++ Assets/EndlessMoving.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class EndlessMoving : MonoBehaviour
 {
 	Vector3 spawnPosition;
-	public float speed = 1.0f;
 	
 	void Start()
 	{
@@ -13,7 +12,7 @@ public class EndlessMoving : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		transform.position += speed * Vector3.left;
+		transform.position += WorldManager.instance.moveSpeed * Vector3.left;
 		if(WorldManager.instance.spawnPosition.x - transform.position.x >= WorldManager.instance.maxMoveDistance)
 		{
 			Destroy(gameObject);
--- Assets/scripts/playerMove.cs
+++ Assets/scripts/playerMove.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class playerMove : MonoBehaviour {
 
+	const float MOVESPEED = 10;
+	const float JUMPFORCE = 300;
+
     bool isAttacking;
     bool isCrounching;
     bool isGrounded;
@@ -21,7 +24,8 @@ public class playerMove : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 
         if (Input.GetKeyDown(KeyCode.X)){
@@ -32,7 +36,7 @@ public class playerMove : MonoBehaviour {
         }
 
         if (Input.GetKeyDown("space") && isGrounded){
-            rb.AddForce(new Vector3(0, 300.0f,0));
+            rb.AddForce(new Vector3(0, JUMPFORCE,0));
         }
 
         if (Input.GetKeyDown(KeyCode.LeftControl)){
@@ -44,19 +48,28 @@ public class playerMove : MonoBehaviour {
 
 
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * 1 * Time.deltaTime;
+        transform.position += move * MOVESPEED * Time.deltaTime;
 
     }
 
+	//void FixedUpdate()
+	//{
+
+	//	if()
+	//}
+
+
     void OnCollisionEnter(Collision other){
         Debug.Log("isGrounded == " + isGrounded);
-        if (other.gameObject.tag == "floor"){
+        if (other.gameObject.tag == "floor")
+		{
             isGrounded = true;
         }
     }
 
     void OnCollisionExit(Collision other){
-       if(other.gameObject.tag == "floor"){
+       if(other.gameObject.tag == "floor")
+		{
             isGrounded = false;
         }
     }
