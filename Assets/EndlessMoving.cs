f6ea3a9a0d8916112b29a8e37a7ce3ca1925acee
Halvor Bakken Smedås <stektpotet@gmail.com>
2016-01-29 23:42:45 +0100

    Made the scrolling world work!
    Now I can look into multiple parts

---
+++ Assets/EndlessMoving.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndlessMoving : MonoBehaviour
+{
+	Vector3 spawnPosition;
+	public float speed = 1.0f;
+	
+	void Start()
+	{
+		spawnPosition = transform.position;
+	}
+
+	void FixedUpdate ()
+	{
+		transform.position += speed * Vector3.left;
+		if(WorldManager.instance.spawnPosition.x - transform.position.x >= WorldManager.instance.maxMoveDistance)
+		{
+			Destroy(gameObject);
+			WorldManager.instance.InstantiatePart();
+		}
+	}
+}
---
+++ Assets/ObjectPooling/WorldManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+
+public class WorldManager : MonoBehaviour {
+
+	public GameObject[] worldPieces;
+
+	public Vector3 spawnPosition;
+
+	public float maxMoveDistance = 150;
+
+	static WorldManager _instance;
+	public static WorldManager instance
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				_instance = FindObjectOfType<WorldManager>();
+			}
+			return _instance;
+		}
+	}
+
+	void Start()
+	{
+		StartGame();
+	}
+
+	void StartGame()
+	{
+	}
+
+
+	public GameObject InstantiatePart()
+	{
+		int piece = Mathf.RoundToInt(Random.value * (worldPieces.Length-1));
+		return InstantiatePart(piece);
+	}
+	/// <summary>
+	/// if it's wanted, you can call upon a specific piece
+	/// </summary>
+	/// <param name="pieceID"></param>
+	public GameObject InstantiatePart(int pieceID)
+	{
+		GameObject newPiece = Instantiate<GameObject>(worldPieces[pieceID]);
+		newPiece.transform.position = spawnPosition;
+		return newPiece;
+	}
+}
