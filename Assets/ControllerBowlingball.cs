8d38b11c9e9ae5f4d8f7b23458e84c0f31573417
Halvor Bakken Smedås <stektpotet@gmail.com>
2016-01-30 20:32:57 +0100

    started adding damage from the thrown objects.

--- Assets/ControllerBowlingball.cs
+++ Assets/ControllerBowlingball.cs
@@ -6,17 +6,25 @@ public class ControllerBowlingball : MonoBehaviour {
     Rigidbody2D rb;
 
     // Use this for initialization
-    void Start () {
-        Debug.Log("spike is alive");
+    void Start ()
+	{
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(new Vector2(600.0f, 30.0f));
         StartCoroutine(WaitAndDie(5.0F));
     }
 
+	void OnCollisionEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			other.GetComponent<Player>().Damage();
+		}
+	}
+
     IEnumerator WaitAndDie(float waitTime)
     {
 
         yield return new WaitForSeconds(waitTime);
-        Object.Destroy(this.gameObject);
+        Destroy(gameObject);
     }
 }
--- Assets/ControllerSpike.cs
+++ Assets/ControllerSpike.cs
@@ -6,14 +6,23 @@ public class ControllerSpike : MonoBehaviour {
     Rigidbody2D rb;
 
 	// Use this for initialization
-	void Start () {
-        Debug.Log("spike is alive");
+	void Start ()
+	{
+
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(new Vector2(600.0f, 1100.0f));
         StartCoroutine(WaitAndDie(5.0F));
     }
-	
-    IEnumerator WaitAndDie(float waitTime)
+
+	void OnCollisionEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			other.GetComponent<Player>().Damage();
+		}
+	}
+
+	IEnumerator WaitAndDie(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
         Object.Destroy(this.gameObject);
