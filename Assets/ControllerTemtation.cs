dc8ad526104ec8086d576f40756a93607508420b
Andreas Weisethaunet <weisethaunet@gmail.com>
2016-01-30 20:28:29 +0100

    added more temtation stuff

---
+++ Assets/ControllerTemtation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerTemtation : MonoBehaviour {
+
+    public GameObject beer;
+    public GameObject DirtyMagazine;
+    public Transform spawnPoint;
+    GameObject child;
+
+    // Use this for initialization
+    void Start () {
+        int randomInt = Random.Range(0,2);
+        if(randomInt == 0){
+            child = Instantiate<GameObject>(beer);
+            child.transform.position = spawnPoint.position;
+            child.transform.parent = spawnPoint.transform;
+        }
+        else{
+            child = Instantiate<GameObject>(beer);
+            child.transform.position = spawnPoint.position;
+            child.transform.parent = spawnPoint;
+
+        }
+    }
+}
