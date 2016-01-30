84c19600ffb5ca7763a0fbe8da770a912cb65f24
Andreas Weisethaunet <weisethaunet@gmail.com>
2016-01-30 01:26:09 +0100

    added devil states, some new studd on localdatabase

---
+++ Assets/DevilAttack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevilAttack : MonoBehaviour {
+
+    
+    public void setState(int state){
+        runAction(state);
+    }
+
+    public void runAction(int state){
+        Debug.Log("In action state = " + state);
+    }
+}
--- Assets/scripts/LocalDatabase.cs
+++ Assets/scripts/LocalDatabase.cs
@@ -3,15 +3,40 @@ using System.Collections;
 
 public class LocalDatabase : MonoBehaviour {
 
+
+    int currentState = 2;
+    int newState = 2;
     public float speed = 50;
+    public int mana = 100;
     private float apocalypseMax = 100;
-    private float apocalypseCurrent = 50;
+    public float apocalypseCurrent = 50;
+    public GameObject devil;
+
+    DevilAttack devilatack;
+
+    public void Start(){
+        devilatack = (DevilAttack)devil.GetComponent(typeof(DevilAttack));
+    }
 
     public float getApocalypseNormalized(){
         return apocalypseCurrent / apocalypseMax;
     }
 
-    public float addApocalypse(float addValue){
-        return apocalypseCurrent += addValue;
+    public void addApocalypse(float addValue){
+        apocalypseCurrent += addValue;
+        checkDevilState(); 
+    }
+
+    float[,] devilAttackList = new float[5, 2] { { -1000, 0.2f }, { 0.2f, .4f }, { .4f, .6f }, { .6f, .8f }, { .8f, 1000 } };
+
+    private void checkDevilState(){
+        if(getApocalypseNormalized() < devilAttackList[currentState, 0]){
+            currentState--;
+            devilatack.setState(currentState);
+        }
+        else if(getApocalypseNormalized() > devilAttackList[currentState, 1]){
+            currentState++;
+            devilatack.setState(currentState);
+        }
     }
 }
